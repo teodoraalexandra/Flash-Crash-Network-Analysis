106a2dbd1c239ac65bb065cd97b9083ca234b8b1
TEODORA DAN <msg.dan@extaccount.com>
2023-03-03 12:53:41 +0100

    Multiprocess Python

--- plots/monte-carlo/csv_mean_creation.py
+++ plots/monte-carlo/csv_mean_creation.py
@@ -2,17 +2,14 @@ from plots.independent.processFile import process
 from plots.independent.pinComputing import compute_pin
 from plots.independent.assortativityComputing import compute_assortativity
 from plots.independent.bipartivityComputing import compute_bipartivity
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import multiprocessing
 import sys
 
-mean_PIN_results = []
-mean_assortativity_results = []
-mean_bipartivity_results = []
 
-for simulationIndex in range(int(sys.argv[1])):
-    index = 1
-    result = process(simulationIndex + 1)
+def task(index, mean_PIN_results, mean_assortativity_results, mean_bipartivity_results, lock):
+    result = process(index + 1)
     PIN_results = []
     assortativity_results = []
     bipartivity_results = []
@@ -38,50 +35,71 @@ for simulationIndex in range(int(sys.argv[1])):
         # plt.title("Day " + str(index))
         # plt.show()
 
-        index += 1
+    with lock:
+        mean_PIN_results.append(PIN_results)
+        mean_assortativity_results.append(assortativity_results)
+        mean_bipartivity_results.append(bipartivity_results)
 
-    mean_PIN_results.append(PIN_results)
-    mean_assortativity_results.append(assortativity_results)
-    mean_bipartivity_results.append(bipartivity_results)
 
-print('Monte Carlo done. Start correlations!\n')
-# Convert the data to a numpy array & compute the average of each column
-mean_PIN_results = np.array(mean_PIN_results, dtype=float)
-mean_PIN_results = np.mean(mean_PIN_results, axis=0)
+if __name__ == '__main__':
+    # Create a PIN list
+    mean_PIN_results = multiprocessing.Manager().list()
+    mean_assortativity_results = multiprocessing.Manager().list()
+    mean_bipartivity_results = multiprocessing.Manager().list()
+    lock = multiprocessing.Lock()
 
-mean_assortativity_results = np.array(mean_assortativity_results, dtype=float)
-mean_assortativity_results = np.mean(mean_assortativity_results, axis=0)
+    # Create three processes for each task using a for loop
+    processes = []
+    for simulationIndex in range(int(sys.argv[1])):
+        process = multiprocessing.Process(target=task, args=(simulationIndex, mean_PIN_results, mean_assortativity_results, mean_bipartivity_results, lock))
+        processes.append(process)
 
-mean_bipartivity_results = np.array(mean_bipartivity_results, dtype=float)
-mean_bipartivity_results = np.mean(mean_bipartivity_results, axis=0)
+    # Start all processes
+    for process in processes:
+        process.start()
 
-# CORRELATION PLOTS
-x_axis_PIN = np.array(mean_PIN_results)
-y_axis_assortativity = np.array(mean_assortativity_results)
-y_axis_bipartivity = np.array(mean_bipartivity_results)
+    # Wait for all processes to finish
+    for process in processes:
+        process.join()
 
-# correlation_matrix_assortativity = np.corrcoef(x_axis_PIN, y_axis_assortativity)
-# print("Correlation matrix PIN-assortativity: \n", correlation_matrix_assortativity)
-# correlation_matrix_bipartivity = np.corrcoef(x_axis_PIN, y_axis_bipartivity)
-# print("Correlation matrix PIN-bipartivity: \n", correlation_matrix_bipartivity)
+    print('Monte Carlo done. Start correlations!\n')
+    # Convert the data to a numpy array & compute the average of each column
+    mean_PIN_results = np.array(mean_PIN_results, dtype=float)
+    mean_PIN_results = np.mean(mean_PIN_results, axis=0)
 
-# The red squares represent the observations
-# The blue line is the regression line
+    mean_assortativity_results = np.array(mean_assortativity_results, dtype=float)
+    mean_assortativity_results = np.mean(mean_assortativity_results, axis=0)
 
-plt.style.use('ggplot')
+    mean_bipartivity_results = np.array(mean_bipartivity_results, dtype=float)
+    mean_bipartivity_results = np.mean(mean_bipartivity_results, axis=0)
 
-# Plot first
-fig1, ax1 = plt.subplots()
-ax1.plot(x_axis_PIN, y_axis_assortativity, linewidth=0, marker='s', label='Data points')
-ax1.set_xlabel('PIN')
-ax1.set_ylabel('ASSORTATIVITY')
-ax1.legend(facecolor='white')
-plt.savefig("plot_correlation_1.png")
+    # CORRELATION PLOTS
+    x_axis_PIN = np.array(mean_PIN_results)
+    y_axis_assortativity = np.array(mean_assortativity_results)
+    y_axis_bipartivity = np.array(mean_bipartivity_results)
 
-# Plot second
-fig2, ax2 = plt.subplots()
-ax2.plot(x_axis_PIN, y_axis_bipartivity, linewidth=0, marker='s', label='Data points')
-ax2.set_xlabel('PIN')
-ax2.set_ylabel('BIPARTIVITY')
-ax2.legend(facecolor='white')
-plt.savefig("plot_correlation_2.png")
+    # correlation_matrix_assortativity = np.corrcoef(x_axis_PIN, y_axis_assortativity)
+    # print("Correlation matrix PIN-assortativity: \n", correlation_matrix_assortativity)
+    # correlation_matrix_bipartivity = np.corrcoef(x_axis_PIN, y_axis_bipartivity)
+    # print("Correlation matrix PIN-bipartivity: \n", correlation_matrix_bipartivity)
+
+    # The red squares represent the observations
+    # The blue line is the regression line
+
+    plt.style.use('ggplot')
+
+    # Plot first
+    fig1, ax1 = plt.subplots()
+    ax1.plot(x_axis_PIN, y_axis_assortativity, linewidth=0, marker='s', label='Data points')
+    ax1.set_xlabel('PIN')
+    ax1.set_ylabel('ASSORTATIVITY')
+    ax1.legend(facecolor='white')
+    plt.savefig("plot_correlation_1.png")
+
+    # Plot second
+    fig2, ax2 = plt.subplots()
+    ax2.plot(x_axis_PIN, y_axis_bipartivity, linewidth=0, marker='s', label='Data points')
+    ax2.set_xlabel('PIN')
+    ax2.set_ylabel('BIPARTIVITY')
+    ax2.legend(facecolor='white')
+    plt.savefig("plot_correlation_2.png")
