c8c3ad0bdcd0ececad3beec73ef334efda8df3d4
Alex Ilies <alex.ilies@dot-core.com>
2024-11-03 21:48:59 +0200

    rerun with new correlations

--- script.sh
+++ script.sh
@@ -19,7 +19,7 @@ export PYTHONPATH=$ROOT_FOLDER
 
 # For loop for our simulations
 # Set the number of times to repeat the commands (number of simulations)
-n=100
+n=75
 days=40
 aggressivity=10
 persons=$1 # This is the total number of the agents
